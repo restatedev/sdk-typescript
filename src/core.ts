458cd259f533d146a3e769ba80b827968914645f
igalshilman <igal@restate.dev>
2023-02-27 16:02:16 +0100

    Add example .proto

--- src/core.ts
+++ src/core.ts
@@ -1,12 +1,14 @@
+import { Message } from "@bufbuild/protobuf";
+
 export interface RestateContext {
   // TODO: what goes here
   dummy: string;
 }
 
-export type RestateMethod = (
+export type RestateMethod<I extends Message<I>, O extends Message<O>> = (
   context: RestateContext,
-  message: any
-) => void | Promise<void>;
+  message: Message<I>
+) => Promise<O>;
 
 /**
  * {
@@ -16,15 +18,20 @@ export type RestateMethod = (
  *     }
  * }
  */
-export interface MethodOpts {
+export interface MethodOpts<I extends Message<I>, O extends Message<O>> {
   method: string;
-  fn: RestateMethod;
+  fn: RestateMethod<I, O>;
 }
 
-export class MethodSpec implements MethodOpts {
-  constructor(readonly method: string, readonly fn: RestateMethod) {}
+export class MethodSpec<I extends Message<I>, O extends Message<O>>
+  implements MethodOpts<I, O>
+{
+  constructor(readonly method: string, readonly fn: RestateMethod<I, O>) {}
 
-  static fromOpts({ method, fn }: MethodOpts): MethodSpec {
+  static fromOpts<I extends Message<I>, O extends Message<O>>({
+    method,
+    fn,
+  }: MethodOpts<I, O>): MethodSpec<I, O> {
     if (fn === undefined || fn === null) {
       throw new Error(`missing method instance for ${method}`);
     }
--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,4 @@
+import { GreetRequest, GreetResponse } from "./generated/example_pb";
 import { Restate, RestateContext } from "./public_api";
 
 const restate = new Restate();
@@ -7,8 +8,10 @@ const restate = new Restate();
 
 restate.bind({
   method: "/dev.restate.Greeter/greet",
-  fn: async function (context: RestateContext, message: any) {
-    console.log(`I don't do a lot just yet.`);
+  fn: async (context: RestateContext, message: GreetRequest) => {
+    console.log(`I don't do a lot just yet. ${message.name}`);
+
+    return new GreetResponse({ greeting: "hello" });
   },
 });
 
