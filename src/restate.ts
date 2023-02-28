e04d41963bf8cdccf79146b78512be390ae8654a
igalshilman <igal@restate.dev>
2023-02-28 10:01:54 +0100

    Fix lint issue

--- src/restate.ts
+++ src/restate.ts
@@ -9,10 +9,7 @@ export interface ServiceOpts {
 }
 
 class HostedGrpcServiceMethod<I, O> {
-  constructor(
-    readonly instance: unknown,
-    readonly method: GrpcServiceMethod<I, O>
-  ) {}
+  constructor(readonly method: GrpcServiceMethod<I, O>) {}
 
   async invoke(inBytes: Uint8Array): Promise<Uint8Array> {
     const input = this.method.inputDecoder(inBytes);
@@ -37,7 +34,7 @@ export class RestateServer {
     const spec = parseService(descriptor, service, instance);
     for (const method of spec.methods) {
       const url = `${spec.packge}.${spec.name}/${method.name}`;
-      this.methods[url] = new HostedGrpcServiceMethod(instance, method);
+      this.methods[url] = new HostedGrpcServiceMethod(method);
       // note that this log will not print all the keys.
       console.log(
         `Registering: ${url}  -> ${JSON.stringify(method, null, "\t")}`
@@ -46,8 +43,16 @@ export class RestateServer {
     return this;
   }
 
+  methodByUrl<I, O>(url: string): HostedGrpcServiceMethod<I, O> | null {
+    const method = this.methods[url];
+    if (method === null || method === undefined) {
+      return null;
+    }
+    return method as HostedGrpcServiceMethod<I, O>;
+  }
+
   public async listen(port: number) {
-    // hello
+    console.log(`Listening on port ${port} ...`);
   }
 
   public async fakeInvoke(url: string, buf: Uint8Array): Promise<Uint8Array> {
