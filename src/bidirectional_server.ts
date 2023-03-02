adfde1c3ed21a5f1e8b7b255965cb4d5d0f666c1
igalshilman <igal@restate.dev>
2023-03-02 12:12:32 +0100

    Add a simple http2 based server that speaks the restate Protocol
    The following is a basic usage example:
    
    ```ts
    public async listen(port: number) {
        console.log(`listening on ${port}...`);
    
        for await (const connection of incomingConnectionAtPort(port)) {
          console.log(`INFO new stream for ${connection.url.path}`);
          const method = this.methodByUrl(connection.url.path);
          if (method === undefined) {
            connection.respond404();
          } else {
            connection.respondOk();
    	this.handleConnection(connection, method);
          }
        }
      }
    ```

---
+++ src/bidirectional_server.ts
@@ -0,0 +1,73 @@
+"use strict";
+
+import http2 from "http2";
+import {
+  RestateDuplexStream,
+  RestateDuplexStreamEventHandler,
+} from "./protocol_stream";
+import { parse as urlparse, Url } from "url";
+import { on } from "events";
+
+export class Connection {
+  constructor(
+    readonly connectionId: bigint,
+    readonly headers: http2.IncomingHttpHeaders,
+    readonly url: Url,
+    readonly stream: http2.ServerHttp2Stream,
+    readonly restate: RestateDuplexStream
+  ) {}
+
+  respond404() {
+    this.stream.respond({
+      "content-type": "application/octet-stream",
+      ":status": 404,
+    });
+    this.stream.end();
+  }
+
+  respondOk() {
+    this.stream.respond({
+      "content-type": "application/octet-stream",
+      ":status": 200,
+    });
+  }
+
+  send(
+    message_type: bigint,
+    message: any,
+    completed?: boolean,
+    requires_ack?: boolean
+  ) {
+    this.restate.send(message_type, message, completed, requires_ack);
+  }
+
+  onMessage(handler: RestateDuplexStreamEventHandler) {
+    this.restate.onMessage(handler);
+  }
+}
+
+export async function* incomingConnectionAtPort(port: number) {
+  const server = http2.createServer();
+
+  server.on("error", (err) => console.error(err));
+  server.listen(port);
+
+  let connectionId = BigInt(1);
+
+  for await (const [stream, headers] of on(server, "stream")) {
+    const s = stream as http2.ServerHttp2Stream;
+    const h = headers as http2.IncomingHttpHeaders;
+    const u: Url = urlparse(h[":path"] ?? "/");
+
+    connectionId += 1n;
+    const connection = new Connection(
+      connectionId,
+      h,
+      u,
+      s,
+      RestateDuplexStream.from(s)
+    );
+
+    yield connection;
+  }
+}
