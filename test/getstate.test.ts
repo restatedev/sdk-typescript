97a30a6b7d93abc6098d2765ca37bb39d225d4af
Giselle van Dongen <giselle.vd@gmail.com>
2023-03-07 12:27:49 +0100

    fix

--- test/getstate.test.ts
+++ test/getstate.test.ts
@@ -21,7 +21,7 @@ export class GreeterService implements Greeter {
     const ctx = restate.useContext(this);
 
     // state
-    const state = (await ctx.getState<string>("STATE")) || 0;
+    const state = (await ctx.getState<string>("STATE")) || "nobody";
     console.log("Current state is " + state);
 
     return GreetResponse.create({ greeting: `Hello ${state}` });
@@ -66,20 +66,18 @@ describe("Greeter/Greeter: Without GetStateEntry", () => {
     });
 });
 
-//   describe("Greeter/Greeter: With GetStateEntry not completed", () => {
-//       it("should call greet", async () => {
-//         TestDriver.setupAndRun(
-//           protoMetadata, "Greeter", new GreeterService(), "/dev.restate.Greeter/Greet", 
-//           [
-//             startMessage(2),
-//             inputMessage(GreetRequest.encode(GreetRequest.create({ name: "Till" })).finish()),
-//             getStateMessage("STATE"),
-//             completionMessage(1, "Francesco")
-//           ])
-//           .then((result) => {
-//             expect(result[0].message_type).toStrictEqual(GET_STATE_ENTRY_MESSAGE_TYPE)
-//             expect(result[0]).toStrictEqual(
-//               getStateMessage("STATE"))
-//           });
-//       });
-//     });
+describe("Greeter/Greeter: With GetStateEntry not completed", () => {
+    it("should call greet", async () => {
+    TestDriver.setupAndRun(
+        protoMetadata, "Greeter", new GreeterService(), "/dev.restate.Greeter/Greet", 
+        [
+        startMessage(2),
+        inputMessage(GreetRequest.encode(GreetRequest.create({ name: "Till" })).finish()),
+        completionMessage(1, "Francesco")
+        ])
+        .then((result) => {
+          const response = GreetResponse.decode(result[0].message.value);
+          expect(response).toStrictEqual(GreetResponse.create({greeting: "Hello Francesco"}))
+        });
+    });
+});
