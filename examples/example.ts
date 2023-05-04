2278bb2a3fc0d6988ac18509298251c98499ab9a
Giselle van Dongen <giselle.vd@gmail.com>
2023-05-04 15:27:10 +0200

    fix test for ext.proto

--- examples/example.ts
+++ examples/example.ts
@@ -1,21 +1,21 @@
 import * as restate from "../src/public_api";
 import {
-  GreetRequest,
-  GreetResponse,
-  Greeter,
+  TestRequest,
+  TestResponse,
+  TestGreeter,
   protoMetadata,
-} from "../src/generated/proto/example";
+} from "../src/generated/proto/test";
 
 /**
  * Example of a service implemented with the Restate Typescript SDK
  * This is a long-running service with two gRPC methods: Greet and MultiWord
  */
-export class GreeterService implements Greeter {
-  async greet(request: GreetRequest): Promise<GreetResponse> {
-    return GreetResponse.create({ greeting: `Hello ${request.name}` });
+export class GreeterService implements TestGreeter {
+  async greet(request: TestRequest): Promise<TestResponse> {
+    return TestResponse.create({ greeting: `Hello ${request.name}` });
   }
 
-  async multiWord(request: GreetRequest): Promise<GreetResponse> {
+  async multiWord(request: TestRequest): Promise<TestResponse> {
     const ctx = restate.useContext(this);
 
     // state
@@ -26,7 +26,7 @@ export class GreeterService implements Greeter {
     await ctx.set("seen", seen);
 
     // return the final response
-    return GreetResponse.create({
+    return TestResponse.create({
       greeting: `YAGM (yet another greeting method) ${request.name}!`,
     });
   }
