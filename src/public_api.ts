ffb934c6535f321e8bb82b58a8440d3ffc90a3d2
Stephan Ewen <sewen@apache.org>
2023-05-10 15:36:16 +0200

    Make 'ServiceOpts' part of the public API.

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,5 +1,6 @@
 export { RestateContext, useContext } from "./restate_context";
 export { RestateServer, createServer } from "./server/restate_server";
+export { ServiceOpts } from "./server/base_restate_server";
 export {
   LambdaRestateServer,
   lambdaApiGatewayHandler,
