bcf5d38b92c79e9f0ff2e522af97aa298ec67b00
Giselle van Dongen <giselle.vd@gmail.com>
2023-05-22 14:43:01 +0200

    Fix inconsistency between lambda and http server

--- src/public_api.ts
+++ src/public_api.ts
@@ -3,6 +3,6 @@ export { RestateServer, createServer } from "./server/restate_server";
 export { ServiceOpts } from "./server/base_restate_server";
 export {
   LambdaRestateServer,
-  lambdaApiGatewayHandler,
+  createLambdaApiGatewayHandler,
 } from "./server/restate_lambda_handler";
 export * as RestateUtils from "./utils/public_utils";
