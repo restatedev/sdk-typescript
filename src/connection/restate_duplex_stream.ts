686eec555a542328ba3ef254cf2ef55c43fd9725
Jack Kleeman <jackkleeman@gmail.com>
2023-05-25 14:43:57 +0100

    Raise max listeners (#62)
    * Raise max listeners
    
    Fix an annoying logline caused by the node runtime creating a ton of listeners briefly
    
    * Catch flush errors

--- src/connection/restate_duplex_stream.ts
+++ src/connection/restate_duplex_stream.ts
@@ -1,12 +1,11 @@
 "use strict";
 
 import stream from "stream";
-import { Message } from "../types/types";
-import { streamEncoder } from "../io/encoder";
-import { streamDecoder } from "../io/decoder";
-import { rlog } from "../utils/logger";
-import { promisify } from "util";
-const pipeline = promisify(stream.pipeline);
+import {Message} from "../types/types";
+import {streamEncoder} from "../io/encoder";
+import {streamDecoder} from "../io/decoder";
+import {rlog} from "../utils/logger";
+import {pipeline} from "stream/promises";
 
 export class RestateDuplexStream {
   // create a RestateDuplex stream from an http2 (duplex) stream.
@@ -22,11 +21,20 @@ export class RestateDuplexStream {
   constructor(
     private readonly sdkInput: stream.Readable,
     private readonly sdkOutput: stream.Writable
-  ) {}
+  ) {
+  }
 
   async send(msgs: Message[]): Promise<void> {
-    const readable = stream.Readable.from(msgs);
-    await pipeline(readable, this.sdkOutput, { end: false });
+    const max = this.sdkOutput.getMaxListeners()
+    // pipeline creates a huge number of listeners, but it is not a leak; they are cleaned up by the time we complete
+    // set to unlimited briefly
+    this.sdkOutput.setMaxListeners(0)
+    await pipeline(
+      stream.Readable.from(msgs),
+      this.sdkOutput,
+      {end: false}
+    )
+    this.sdkOutput.setMaxListeners(max)
   }
 
   end() {
