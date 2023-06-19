1fd9521d2317c61ec38f99c5152f4a93f97c3e0b
Stephan Ewen <sewen@apache.org>
2023-06-19 16:04:46 +0200

    Change logic in RestateHttp2Connection to ensure that events are guaranteed to be delivered to consumers.
    Previously, depending when 'input closed' or even 'error' occurred, it could be that there was no event listener, yet.
    This change ensures that events are always delivered to new consumers.

--- src/connection/http_connection.ts
+++ src/connection/http_connection.ts
@@ -23,27 +23,61 @@ export class RestateHttp2Connection implements Connection {
 
   // --------------------------------------------------------------------------
 
-  private _buffer: Message[] = [];
+  private _outputBuffer: Message[] = [];
+
+  // consumer handling
   private currentConsumer: RestateStreamConsumer | null = null;
+  private inputBuffer: Message[] = [];
+  private consumerError?: Error;
+  private consumerInputClosed = false;
 
   constructor(
     private readonly sdkInput: stream.Readable,
     private readonly sdkOutput: stream.Writable
   ) {
-    // install default logging for errors
+    // install error listeners on the stream
+    const errorHandler = (e: Error) => {
+      this.consumerError = e;
+      if (this.currentConsumer) {
+        this.currentConsumer.handleStreamError(e);
+      }
+    };
     this.sdkInput.on("error", (e: Error) => {
-      rlog.error(
-        "Error in input stream (from Restate to SDK/Service): " + e.message
-      );
+      rlog.error("Error in input stream (Restate to Service): " + e.message);
       rlog.error(e.stack);
+      errorHandler(e);
     });
     this.sdkOutput.on("error", (e: Error) => {
-      rlog.error(
-        "Error in output stream (from SDK/Service to  Restate): " + e.message
-      );
+      rlog.error("Error in output stream (Service to  Restate): " + e.message);
       rlog.error(e.stack);
+      errorHandler(e);
+    });
+
+    // remember and forward close events
+    this.sdkInput.on("close", () => {
+      rlog.debug("Stream: Input closed");
+      this.consumerInputClosed = true;
+      if (this.currentConsumer) {
+        this.currentConsumer.handleInputClosed();
+      }
+    });
+
+    // remember and forward messages
+    this.sdkInput.on("data", (m: Message) => {
+      // deliver message, if we have a consumer. otherwise buffer the message.
+      if (this.currentConsumer) {
+        if (this.currentConsumer.handleMessage(m)) {
+          this.removeCurrentConsumer();
+        }
+      } else {
+        this.inputBuffer.push(m);
+      }
     });
   }
+
+  // --------------------------------------------------------------------------
+  //  input stream handling
+  // --------------------------------------------------------------------------
 
   /**
    * Pipes the messages from this connection to the given consumer. The consumer
@@ -59,52 +93,53 @@ export class RestateHttp2Connection implements Connection {
       throw new Error("Already piping to a consumer");
     }
 
-    const handleMessage = (m: Message) => {
-      const done = consumer.handleMessage(m);
-      if (done) {
-        this.removeCurrentConsumer();
-      }
-      return done;
-    };
-    const handleInputClosed = consumer.handleInputClosed.bind(consumer);
-    const handleStreamError = consumer.handleStreamError.bind(consumer);
-    this.currentConsumer = {
-      handleMessage,
-      handleInputClosed,
-      handleStreamError,
-    };
+    this.currentConsumer = consumer;
 
-    this.sdkInput.on("data", handleMessage);
-    this.sdkInput.on("close", handleInputClosed);
-    this.sdkInput.on("error", handleStreamError);
-    this.sdkOutput.on("error", handleStreamError);
+    // propagate pre-existing information
+    if (this.consumerError) {
+      consumer.handleStreamError(this.consumerError);
+    }
+    if (this.consumerInputClosed) {
+      consumer.handleInputClosed();
+    }
+
+    const input = this.inputBuffer;
+    if (input.length > 0) {
+      let i = 0;
+      while (i < input.length) {
+        const done = consumer.handleMessage(input[i]);
+        i++;
+        if (done) {
+          // consumer is done
+          this.removeCurrentConsumer();
+          break;
+        }
+      }
+      this.inputBuffer = i === input.length ? [] : this.inputBuffer.slice(i);
+    }
   }
 
   /**
-   * Removes the current consumer, if there is one. Otherwise does nothing.
+   * Removes the current consumer, if there is one.
    */
   public removeCurrentConsumer(): void {
-    if (this.currentConsumer === null) {
-      return;
-    }
-    const c = this.currentConsumer;
     this.currentConsumer = null;
-    this.sdkInput.removeListener("data", c.handleMessage);
-    this.sdkInput.removeListener("close", c.handleInputClosed);
-    this.sdkInput.removeListener("error", c.handleStreamError);
-    this.sdkOutput.removeListener("error", c.handleStreamError);
   }
 
+  // --------------------------------------------------------------------------
+  //  output stream handling
+  // --------------------------------------------------------------------------
+
   public buffer(msg: Message): void {
-    this._buffer.push(msg);
+    this._outputBuffer.push(msg);
   }
 
   public async flush(): Promise<void> {
-    if (this._buffer.length == 0) {
+    if (this._outputBuffer.length == 0) {
       return;
     }
-    const buffer = this._buffer;
-    this._buffer = [];
+    const buffer = this._outputBuffer;
+    this._outputBuffer = [];
 
     const max = this.sdkOutput.getMaxListeners();
     // pipeline creates a huge number of listeners, but it is not a leak; they are cleaned up by the time we complete
