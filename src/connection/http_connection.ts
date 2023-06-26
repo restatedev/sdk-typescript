2c174b4534c676806f66c46849d0233fec5449bf
Stephan Ewen <sewen@apache.org>
2023-06-26 12:23:37 +0200

    Fix problems with concurrent flushes (#104)
    Flushes now are queued and triggered as previous flushes complete.
    Also improve comments and style in RestateHttp2Connection

--- src/connection/http_connection.ts
+++ src/connection/http_connection.ts
@@ -1,13 +1,35 @@
 "use strict";
 
 import stream, { EventEmitter } from "stream";
-import { pipeline } from "stream/promises";
+import { pipeline, finished } from "stream/promises";
 import { streamEncoder } from "../io/encoder";
 import { streamDecoder } from "../io/decoder";
 import { Connection, RestateStreamConsumer } from "./connection";
 import { Message } from "../types/types";
 import { rlog } from "../utils/logger";
 
+/**
+ * A duplex stream with Restate Messages over HTTP2.
+ *
+ * This stream handles the following concerns:
+ *
+ * (1) encoding and decoding of messages from  and from raw bytes
+ *
+ * (2) buffering the outgoing messages, because the call sites that produce (potentially) large
+ *     messages might not await their transfer. Aside from the fact that we achieve better pipelining
+ *     that way, we also simply cannot guarantee that users of the Restate SDK actually await the
+ *     relevant async API methods.
+ *
+ *     This stream essentially buffers messages and, upon flush, sends them asynchronously, as the
+ *     stream has availability. Flush requests queue up, if new data gets flushed while the previous
+ *     data is still being sent.
+ *
+ * (3) Input messages can be pipelined to a sequence of consumers. For example, first to a journal,
+ *     and afterwards to the state machine.
+ *
+ * (4) Handling the relevant stream events for errors and consolidating them to one error handler, plus
+ *     notifications for cleanly closed input (to trigger suspension).
+ */
 export class RestateHttp2Connection implements Connection {
   /**
    * create a RestateDuplex stream from an http2 (duplex) stream.
@@ -23,7 +45,9 @@ export class RestateHttp2Connection implements Connection {
 
   // --------------------------------------------------------------------------
 
-  private _outputBuffer: Message[] = [];
+  // output stream handling
+  private outputBuffer: Message[] = [];
+  private flushQueueTail: Promise<void> = Promise.resolve();
 
   // consumer handling
   private currentConsumer: RestateStreamConsumer | null = null;
@@ -124,6 +148,7 @@ export class RestateHttp2Connection implements Connection {
       consumer.handleInputClosed();
     }
 
+    // pipe the buffered input messages, if we buffered some before the consumer was registered
     const input = this.inputBuffer;
     if (input.length > 0) {
       let i = 0;
@@ -131,7 +156,6 @@ export class RestateHttp2Connection implements Connection {
         const done = consumer.handleMessage(input[i]);
         i++;
         if (done) {
-          // consumer is done
           this.removeCurrentConsumer();
           break;
         }
@@ -151,28 +175,64 @@ export class RestateHttp2Connection implements Connection {
   //  output stream handling
   // --------------------------------------------------------------------------
 
+  /**
+   * Adds a message to the output buffer, but does not yet attempt to send it.
+   * Messages are only actually sent when {@link flush} is called.
+   */
   public buffer(msg: Message): void {
-    this._outputBuffer.push(msg);
+    this.outputBuffer.push(msg);
   }
 
+  /**
+   * Flushes all currently buffered messages to the output stream.
+   * The returned promise resolves when the data has been fully flushed.
+   *
+   * If another flush operation is still ongoing (for example due to stream backpressure), then
+   * this flush will start executing once the pending request(s) finished.
+   */
   public async flush(): Promise<void> {
-    if (this._outputBuffer.length == 0) {
+    if (this.outputBuffer.length == 0) {
       return;
     }
-    const buffer = this._outputBuffer;
-    this._outputBuffer = [];
 
-    const max = this.sdkOutput.getMaxListeners();
-    // pipeline creates a huge number of listeners, but it is not a leak; they are cleaned up by the time we complete
-    // set to unlimited briefly
-    this.sdkOutput.setMaxListeners(0);
-    await pipeline(stream.Readable.from(buffer), this.sdkOutput, {
-      end: false,
-    });
-    this.sdkOutput.setMaxListeners(max);
+    const data = this.outputBuffer;
+    this.outputBuffer = [];
+
+    // this adds a new flush to the tail of the queue. If no flush is currently happening
+    // (which is mostly the case) then the promise is already resolved and this flush starts
+    // immediately.
+    // NOTE: We do not add a '.catch()' because we want that, after one failure, all further flushes
+    //       are skipped and promises rejected
+    this.flushQueueTail = this.flushQueueTail.then(() => this.doFlush(data));
+    return this.flushQueueTail;
   }
 
-  public end() {
+  private async doFlush(data: Message[]): Promise<void> {
+    // pipeline creates a huge number of listeners, but it is not a leak; they are cleaned up by the time we complete
+    // set to unlimited briefly
+    const max = this.sdkOutput.getMaxListeners();
+    try {
+      this.sdkOutput.setMaxListeners(0);
+      await pipeline(stream.Readable.from(data), this.sdkOutput, {
+        end: false,
+      });
+    } finally {
+      this.sdkOutput.setMaxListeners(max);
+    }
+  }
+
+  /**
+   * Ends the stream, awaiting pending flushes.
+   */
+  public async end(): Promise<void> {
+    // ensure everything is written before we close the stream
+    await this.flush();
     this.sdkOutput.end();
+
+    // this here *would* be nice to surface errors (if the stream is broken), and
+    // it normally should (like on file streams), but it seems that the way the node http2
+    // stream is implemented, it never reports errors here
+    await finished(this.sdkOutput);
+    await finished(this.sdkInput);
   }
 }
