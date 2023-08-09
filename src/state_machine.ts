2bc2558205e7742c35064538a4dc4856998dcdc8
Stephan Ewen <stephan@restate.dev>
2023-08-09 13:39:58 +0200

    Add license headers to all files.
    This also removes the "use strict" found in some files, which is redundant, because it is implied by the
    typescript compiler under our configuration.

--- src/state_machine.ts
+++ src/state_machine.ts
@@ -1,4 +1,13 @@
-"use strict";
+/*
+ * Copyright (c) 2023 - Restate Software, Inc., Restate GmbH
+ *
+ * This file is part of the Restate SDK for Node.js/TypeScript,
+ * which is released under the MIT license.
+ *
+ * You can find a copy of the license in file LICENSE in the root
+ * directory of this repository or package, or at
+ * https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+ */
 
 import * as p from "./types/protocol";
 import { RestateGrpcContextImpl } from "./restate_context_impl";
