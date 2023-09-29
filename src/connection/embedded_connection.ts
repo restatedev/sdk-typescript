ecf767515a13ee3d4f2670f78053528db07d7c3b
igalshilman <igal@restate.dev>
2023-09-29 15:44:36 +0200

    Add license headers

--- src/connection/embedded_connection.ts
+++ src/connection/embedded_connection.ts
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2023 - Restate Software, Inc., Restate GmbH
+ *
+ * This file is part of the Restate SDK for Node.js/TypeScript,
+ * which is released under the MIT license.
+ *
+ * You can find a copy of the license in file LICENSE in the root
+ * directory of this repository or package, or at
+ * https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+ */
+
 import { RemoteContext } from "../generated/proto/services";
 import { encodeMessages } from "../io/encoder";
 import { Message } from "../types/types";
--- src/embedded/api.ts
+++ src/embedded/api.ts
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2023 - Restate Software, Inc., Restate GmbH
+ *
+ * This file is part of the Restate SDK for Node.js/TypeScript,
+ * which is released under the MIT license.
+ *
+ * You can find a copy of the license in file LICENSE in the root
+ * directory of this repository or package, or at
+ * https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+ */
+
 import { RpcContext } from "../restate_context";
 import { doInvoke } from "./invocation";
 import { wrapHandler } from "./handler";
