90510b35eb9f4e0d6a948fe01127983cb78e1e3d
slinkydeveloper <francescoguard@gmail.com>
2023-10-16 12:37:20 +0200

    Feedback

--- src/server/base_restate_server.ts
+++ src/server/base_restate_server.ts
@@ -452,7 +452,7 @@ async function dispatchKeyedEventHandler(
 ): Promise<Empty> {
   const ctx = new RpcContextImpl(origCtx);
   const key = req.key;
-  if (key.length === 0) {
+  if (!key) {
     // we throw a terminal error here, because this cannot be patched by updating code:
     // if the request is wrong (missing a key), the request can never make it
     throw new TerminalError(
