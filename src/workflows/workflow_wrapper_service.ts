fce5e5baa5b703dfe8be696b8a6f25369beaecd2
Stephan Ewen <stephan@restate.dev>
2024-02-06 20:49:46 +0100

    Add license headers to new files

--- src/workflows/workflow_wrapper_service.ts
+++ src/workflows/workflow_wrapper_service.ts
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2023-2024 - Restate Software, Inc., Restate GmbH
+ *
+ * This file is part of the Restate SDK for Node.js/TypeScript,
+ * which is released under the MIT license.
+ *
+ * You can find a copy of the license in file LICENSE in the root
+ * directory of this repository or package, or at
+ * https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+ */
+
 import * as restate from "../public_api";
 import * as wf from "./workflow";
 import * as wss from "./workflow_state_service";
