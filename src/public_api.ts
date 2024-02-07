8a301ad518c6d14b7b4ab565c53c43057d2c7df1
Stephan Ewen <stephan@restate.dev>
2024-02-07 02:29:55 +0100

    Make service registration extensible.
    This introduces the 'ServiceBundle' interface to pass a set of services to the
    server/endpoint.
    
    To make this more clean, this also introduces the 'ServiceEndpoint' interface shared
    by the HTTP/2 server and LambdaEventHandler to extract the common registration parts.

--- src/public_api.ts
+++ src/public_api.ts
@@ -30,7 +30,11 @@ export {
   SendClient,
 } from "./types/router";
 export { RestateServer, createServer } from "./server/restate_server";
-export { ServiceOpts } from "./server/base_restate_server";
+export {
+  ServiceOpts,
+  ServiceBundle,
+  ServiceEndpoint,
+} from "./server/base_restate_server";
 export {
   LambdaRestateServer,
   createLambdaApiGatewayHandler,
