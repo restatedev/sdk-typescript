9ed4ae2f9223ed5818bf3bb518f8c9014fe6752c
igalshilman <igal@restate.dev>
2024-03-20 13:08:15 +0100

    [ingress] Add a typed client
    This commit adds the first version of a typed client.
    See examples/ingress.ts for a usage example.

---
+++ examples/ingress.ts
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2023-2023 - Restate Software, Inc., Restate GmbH
+ *
+ * This file is part of the Restate SDK for Node.js/TypeScript,
+ * which is released under the MIT license.
+ *
+ * You can find a copy of the license in file LICENSE in the root
+ * directory of this repository or package, or at
+ * https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+ */
+
+/* eslint-disable no-console */
+
+import * as restate from "../src/public_api";
+import type { CounterObject, GreeterService } from "./example";
+
+const Greeter: GreeterService = { path: "greeter" };
+const Counter: CounterObject = { path: "counter" };
+
+const ingress = restate.ingress.connect({ url: "http://localhost:8080" });
+
+const simpleCall = async (name: string) => {
+  const greeter = ingress.service(Greeter);
+  const greeting = await greeter.greet(name);
+
+  console.log(greeting);
+};
+
+const objectCall = async (name: string) => {
+  const couter = ingress.object(Counter, name);
+  const count = await couter.count();
+
+  console.log(`The count for ${name} is ${count}`);
+};
+
+const idempotentCall = async (name: string, idempotencyKey: string) => {
+  const greeter = ingress.service(Greeter);
+
+  // send the request with the idempotent key, and ask restate
+  // to remember that key for 3 seconds.
+  const greeting = await greeter.greet(
+    name,
+    restate.ingress.Opts.from({ idempotencyKey, retain: 3 })
+  );
+
+  console.log(greeting);
+};
+
+const customHeadersCall = async (name: string) => {
+  const greeter = ingress.service(Greeter);
+
+  const greeting = await greeter.greet(
+    name,
+    restate.ingress.Opts.from({ headers: { "x-bob": "1234" } })
+  );
+
+  console.log(greeting);
+};
+
+const globalCustomHeaders = async (name: string) => {
+  const ingress = restate.ingress.connect({
+    url: "http://localhost:8080",
+    headers: { Authorization: "Bearer mytoken123" },
+  });
+
+  const greeting = await ingress.service(Greeter).greet(name);
+
+  console.log(greeting);
+};
+
+// Before running this example, make sure
+// to run and register `greeter` and `counter` services.
+//
+// to run them, run:
+//
+// 1. npm run example
+// 2. make sure that restate is running
+// 3. restate deployment add localhost:9080
+
+Promise.resolve()
+  .then(() => simpleCall("bob"))
+  .then(() => objectCall("bob"))
+  .then(() => objectCall("mop"))
+  .then(() => idempotentCall("joe", "idemp-1"))
+  .then(() => idempotentCall("joe", "idemp-1"))
+  .then(() => customHeadersCall("bob"))
+  .then(() => globalCustomHeaders("bob"))
+  .catch((e) => console.error(e));
