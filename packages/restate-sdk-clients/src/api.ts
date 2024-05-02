3456252f06e440c0ea944da15a621cb7a0af9da2
igalshilman <igal@restate.dev>
2024-05-02 12:49:20 +0200

    [clients] Move optional type parameters to the end for the send* variants

--- packages/restate-sdk-clients/src/api.ts
+++ packages/restate-sdk-clients/src/api.ts
@@ -22,14 +22,14 @@ export interface Ingress {
   /**
    * Create a client from a {@link ServiceDefinition}.
    */
-  serviceSendClient<P extends string, M>(
+  serviceSendClient<M, P extends string = string>(
     opts: ServiceDefinition<P, M>
   ): IngressSendClient<M>;
 
   /**
    * Create a client from a {@link VirtualObjectDefinition}.
    */
-  objectSendClient<P extends string, M>(
+  objectSendClient<M, P extends string = string>(
     opts: VirtualObjectDefinition<P, M>,
     key: string
   ): IngressSendClient<M>;
