45e8d0770e007f20f21f0529cc54b94bae726c90
Muhamad Awad <muhamad@restate.dev>
2025-06-05 12:45:31 +0200

    [e2e] Support unlimited concurrent streams
    Summary:
    - Also enable heapdump with USR2 signal
    - Show number of inflight invocation periodically

--- packages/restate-e2e-services/src/app.ts
+++ packages/restate-e2e-services/src/app.ts
@@ -25,6 +25,23 @@ import "./test_utils.js";
 import "./kill.js";
 import "./virtual_object_command_interpreter.js";
 import * as http2 from "http2";
+import * as heapdump from "heapdump";
+import path from "path";
+
+// Optional: trigger a heap snapshot on signal
+process.on("SIGUSR2", () => {
+  const filename = path.join("/opt", `heap-${Date.now()}.heapsnapshot`);
+  // eslint-disable-next-line no-console
+  console.log(`Writing snapshot to ${filename}...`);
+  heapdump.writeSnapshot(filename, (err, filename) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+    // eslint-disable-next-line no-console
+    else console.log(`Heap snapshot written to ${filename}`);
+  });
+});
 
 import { REGISTRY } from "./services.js";
 
@@ -35,14 +52,33 @@ const fqdns = new Set(process.env.SERVICES.split(","));
 const endpoint = restate.endpoint();
 REGISTRY.register(fqdns, endpoint);
 
+const settings: http2.Settings = {};
+if (process.env.MAX_CONCURRENT_STREAMS) {
+  settings.maxConcurrentStreams = parseInt(process.env.MAX_CONCURRENT_STREAMS);
+}
+
 if (process.env.E2E_REQUEST_SIGNING) {
   endpoint.withIdentityV1(...process.env.E2E_REQUEST_SIGNING.split(","));
 }
 
-const server = http2.createServer(endpoint.http2Handler());
-const maxConcurrentStreams = parseInt(
-  process.env.MAX_CONCURRENT_STREAMS || "256"
-);
-server.updateSettings({ maxConcurrentStreams: maxConcurrentStreams });
+let INFLIGHT_REQUESTS = 0;
+
+const handler = endpoint.http2Handler();
+const server = http2.createServer((req, res) => {
+  INFLIGHT_REQUESTS++;
+  res.once("close", () => {
+    INFLIGHT_REQUESTS--;
+  });
+  handler(req, res);
+});
+
+setInterval(() => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `${new Date().toISOString()}: Inflight requests: ${INFLIGHT_REQUESTS}`
+  );
+}, 30 * 1000);
+
+server.updateSettings(settings);
 
 server.listen(parseInt(process.env.PORT || "9080"));
