d381a26012b9ce1061bc80f08226fd6d5900ce78
Nik Nasr <nik@restate.dev>
2025-07-28 17:39:32 +0100

    add serve
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- packages/restate-sdk/src/node.ts
+++ packages/restate-sdk/src/node.ts
@@ -18,6 +18,11 @@ import { withOptions } from "./endpoint/withOptions.js";
 /**
  * Creates an HTTP/2 request handler for the provided services.
  *
+ * @example
+ * ```
+ * const httpServer = http2.createServer(createEndpointHandler({ services: [myService] }));
+ * httpServer.listen(port);
+ * ```
  * @param {EndpointOptions} options - Configuration options for the endpoint handler.
  * @returns An HTTP/2 request handler function.
  */
@@ -26,4 +31,26 @@ export function createEndpointHandler(options: EndpointOptions) {
     new NodeEndpoint(),
     options
   ).http2Handler();
+}
+
+interface ServeOptions extends EndpointOptions {
+  port?: number;
+}
+
+/**
+ * Serves this Restate services as HTTP2 server, listening to the given port.
+ *
+ * If the port is undefined, this method will use the port set in the `PORT`
+ * environment variable. If that variable is undefined as well, the method will
+ * default to port 9080.
+ *
+ * The returned promise resolves with the bound port when the server starts listening, or rejects with a failure otherwise.
+ *
+ * If you need to manually control the server lifecycle, we suggest to manually instantiate the http2 server and use {@link createEndpointHandler}.
+ *
+ * @param {ServeOptions} options - Configuration options for the endpoint handler.
+ * @returns a Promise that resolves with the bound port, or rejects with a failure otherwise.
+ */
+export function serve({ port, ...options }: ServeOptions) {
+  return withOptions<RestateEndpoint>(new NodeEndpoint(), options).listen(port);
 }
