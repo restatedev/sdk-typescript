41cc2888fe278e7bbe8b7e34dbf57d76954ade89
slinkydeveloper <me@slinkydeveloper.com>
2025-08-13 17:21:30 +0200

    Format

--- packages/restate-sdk/src/endpoint/withOptions.ts
+++ packages/restate-sdk/src/endpoint/withOptions.ts
@@ -3,7 +3,13 @@ import type { EndpointOptions } from "./types.js";
 
 export function withOptions<E extends RestateEndpointBase<E>>(
   endpoint: E,
-  { identityKeys, defaultServiceOptions, logger, services, defaultJournalEntryCodec }: EndpointOptions
+  {
+    identityKeys,
+    defaultServiceOptions,
+    logger,
+    services,
+    defaultJournalEntryCodec,
+  }: EndpointOptions
 ): E {
   let endpointWithOptions = endpoint;
   if (identityKeys && identityKeys.length > 0) {
@@ -15,9 +21,9 @@ export function withOptions<E extends RestateEndpointBase<E>>(
     );
   }
   if (defaultJournalEntryCodec) {
-      endpointWithOptions = endpointWithOptions.defaultJournalEntryCodec(
-          defaultJournalEntryCodec
-      );
+    endpointWithOptions = endpointWithOptions.defaultJournalEntryCodec(
+      defaultJournalEntryCodec
+    );
   }
   if (logger) {
     endpointWithOptions = endpointWithOptions.setLogger(logger);
