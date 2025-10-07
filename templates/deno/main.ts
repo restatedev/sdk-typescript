69ec5563312a894405c514c2706e9cf814ac1ddf
Nik Nasr <nik@restate.dev>
2025-10-07 18:11:44 +0100

    add node deno
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ templates/deno/main.ts
@@ -0,0 +1,32 @@
+import * as restate from "@restatedev/restate-sdk/fetch";
+import { serde } from "@restatedev/restate-sdk-zod";
+
+import { z } from "zod";
+
+const Greeting = z.object({
+  name: z.string(),
+});
+
+const GreetingResponse = z.object({
+  result: z.string(),
+});
+
+export const greeter = restate.service({
+  name: "Greeter",
+  handlers: {
+    greet: restate.createServiceHandler(
+      { input: serde.zod(Greeting), output: serde.zod(GreetingResponse) },
+      async (ctx: restate.Context, { name }) => {
+        // Respond to caller
+        return { result: `You said hi to ${name}!` };
+      }
+    ),
+  },
+});
+
+const handler = restate.createEndpointHandler({
+  services: [greeter],
+  bidirectional: true,
+});
+
+Deno.serve({ port: 9080 }, handler);
