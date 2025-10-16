3224308bb4adafc399c75a77c29ba79d12dc71be
Nik Nasr <nik@restate.dev>
2025-10-16 17:21:32 +0100

    add bun
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- packages/examples/bun/src/index.ts
+++ packages/examples/bun/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Example: bun");
+import * as restate from "@restatedev/restate-sdk/fetch";
+import { serde } from "@restatedev/restate-sdk-zod";
+
+import { z } from "zod";
+
+const Greeting = z.object({
+  name: z.string(),
+});
+
+const GreetingResponse = z.object({
+  result: z.string(),
+});
+
+const greeter = restate.service({
+  name: "Greeter",
+  handlers: {
+    greet: restate.createServiceHandler(
+      { input: serde.zod(Greeting), output: serde.zod(GreetingResponse) },
+      async (ctx: restate.Context, { name }) => {
+        // Respond to caller
+        return { result: `You said hi to ${name}!` };
+      },
+    ),
+  },
+});
+
+Bun.serve({
+  fetch: restate.createEndpointHandler({ services: [greeter] }),
+  port: 9080,
+});
