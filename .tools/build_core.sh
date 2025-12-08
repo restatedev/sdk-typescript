7463827f65e39490fb3d20fa016a848b44cb2599
Nik Nasr <nik@restate.dev>
2025-12-08 13:37:31 +0000

    fix .tools paths
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- .tools/build_core.sh
+++ .tools/build_core.sh
@@ -5,6 +5,6 @@ PROJECT_ROOT="$(dirname "$SELF_PATH")/.."
 
 pushd $PROJECT_ROOT/sdk-shared-core-wasm-bindings
 wasm-pack build --target web
-npx wasm-pack-inline ./pkg --dir ../packages/restate-sdk/src/endpoint/handlers/vm --name sdk_shared_core_wasm_bindings
-wasm-pack build --target bundler -d ../packages/restate-sdk-cloudflare-workers/patches/vm
+npx wasm-pack-inline ./pkg --dir ../packages/libs/restate-sdk/src/endpoint/handlers/vm --name sdk_shared_core_wasm_bindings
+wasm-pack build --target bundler -d ../packages/libs/restate-sdk-cloudflare-workers/patches/vm
 popd
